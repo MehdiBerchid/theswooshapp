a263495700f0234d7108f151f04a85ef73d8d8ba
Ghoul Mehdi <ghoulmehdi@MacBook-Pro-de-Ghoul.local>
2018-06-22 03:45:56 +0100

    building model

--- the-swosh-app/the-swosh-app/Controller/LeagueVC.swift
+++ the-swosh-app/the-swosh-app/Controller/LeagueVC.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
- 
+    
+    var player: Player!
+    
+    
+    
+    
+    @IBOutlet weak var nextbtn: Borderbutton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        player = Player()
     }
     @IBAction func onNexttapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
-        
     }
 
+    @IBAction func Menstapped(_ sender: Any) {
+        selectedleahue(leaguetype: "Mens")
+        
+    }
+    
+    @IBAction func womenstapped(_ sender: Any) {
+        selectedleahue(leaguetype: "Womens")
+        
+    }
+    
+    @IBAction func co_edtapped(_ sender: Any) {
+        selectedleahue(leaguetype: "COEd")
+        
+    }
+    
+    func selectedleahue(leaguetype: String) {
+        player.desiredleague = leaguetype
+        nextbtn.isEnabled = true
+    }
+    
 }
